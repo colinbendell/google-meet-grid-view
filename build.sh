09127c714dc917a6bccfd911be3c6c5c00624d74
colin <colin@bendell.ca>
2020-07-07 17:03:20 -0400

    Move manifest to the root to allow unpacked exetension use

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 rm -rf build/*
-cp -r extension build/extension
-cp grid.user.js build/extension
-cp extension.js build/extension
-cp extension.css build/extension
+mkdir -p build
+cp -r extension build
+cp manifest.json build
+cp grid.user.js build
+cp extension.js build
+cp extension.css build
 rm build/extension/screenshot.png
 rm build/extension/small-promo.png
 rm build/extension/large-promo.png
 rm build/extension/marquee-promo.png
-zip -r -j build/extension.zip build/extension
+cd build
+zip -r extension.zip *
